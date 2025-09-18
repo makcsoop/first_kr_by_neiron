524229b5f8a1c93882c122cba2a646b9750c0915
Quaant <egor.zhulin333@gmail.com>
2025-09-18 21:19:42 +0300

    final

--- solution.php
+++ solution.php
@@ -5,13 +5,31 @@
  * Задача 1: Анализатор оценок
  */
 function analyzeGrades(array $grades): array {
-    // TODO: Реализуйте эту функцию
+    $avg = 0; 
+    $max = -1; 
+    $min = 6; 
+    $c_e = 0; 
+    $c_u = 0; 
+    for ($i = 0; $i < count($grades); $i++){ 
+        $avg += $grades[$i]; 
+        $max = max($max, $grades[$i]); 
+        $min = min($min, $grades[$i]); 
+        if ($grades[$i] === 5){ 
+            $c_e++; 
+        }
+        if ($grades[$i] === 1 or $grades[$i] === 2){
+            $c_u++; 
+        }
+
+    }
+
+    
     return [
-        'average' => 0.0,
-        'max' => 0,
-        'min' => 0,
-        'count_excellent' => 0,
-        'count_unsatisfactory' => 0
+        'average' => ($avg / count($grades)),
+        'max' => $max,
+        'min' => $min,
+        'count_excellent' => $c_e,
+        'count_unsatisfactory' => $c_u 
     ];
 }
 
@@ -19,15 +37,30 @@ function analyzeGrades(array $grades): array {
  * Задача 2: Поиск уникальных элементов
  */
 function findUniqueElements(array $numbers): array {
-    // TODO: Реализуйте эту функцию
-    // Не используйте array_unique!
-    return [];
+    $res = []; 
+    for ($i = 0; $i < count($numbers); $i++){ 
+        if (!in_array($numbers[$i], $res)){ 
+            array_push($res, $numbers[$i]); 
+        }
+    }
+    return $res;
 }
 
 /**
  * Задача 3: Фильтр товаров по цене
  */
 function filterProductsByPrice(array $products, float $minPrice, float $maxPrice): array {
-    // TODO: Реализуйте эту функцию
-    return [];
+    $res = [];
+    for ($i = 0; $i < count($products); $i++){ 
+
+        if ($products[$i]['price'] == $minPrice){ 
+            array_push($res, $products[$i]);
+        }
+        if ($products[$i]['price'] == $maxPrice){ 
+            array_push($res, $products[$i]);
+        }
+    }
+
+
+    return $res;
 }
